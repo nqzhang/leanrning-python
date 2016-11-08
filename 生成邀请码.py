dda9ab30b7da6317f48afcd2447986aaf5055e1d
nqzhang <898611548@qq.com>
2016-11-08 10:45:27 +0800

    11.8

--- 生成邀请码.py
+++ 生成邀请码.py
@@ -1,15 +1,28 @@
 import string
 import random
+import mysql.connector
+import redis
+
+config = {
+  'user': 'root',
+  'password': '11QQqqWW',
+  'host': '127.0.0.1',
+  'database': 'learningpython',
+  'raise_on_warnings': True,
+}
+cnx = mysql.connector.connect(**config)
+cursor = cnx.cursor()
+r = redis.StrictRedis(host='localhost', port=6379, db=0)
+
 def GenInviteCode():
-    i = 0
-    InviteCode = ''
-    while i <= 10:
-        str = random.choice(string.ascii_lowercase)
-        InviteCode += str
-        i += 1
-        if i== 10:
-            print(InviteCode)
+    str = ''.join(random.sample(string.ascii_lowercase, 15))
+    add_code = ("insert into invite_code values ('%s')"%str)
+    cursor.execute((add_code))
+    r.lpush('invite_code',str)
+
 n=0
 while n <= 200:
     GenInviteCode()
     n += 1
+
+cnx.commit()
---
+++ 统计单词个数.py
@@ -0,0 +1,2 @@
+with open('text/news.txt','r') as f:
+    print(''.join(f.readlines()))
